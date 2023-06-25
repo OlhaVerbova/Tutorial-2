f13c2a57765aa81df51ac682fde55aba812e35bb
OlhaVerbova <137227019+OlhaVerbova@users.noreply.github.com>
2023-06-25 21:43:26 +0300

    2

--- hw2.py
+++ hw2.py
@@ -3,31 +3,46 @@ operand = None # 0, 1, 2, 3..
 operator = None # + - * / 
 wait_for_number = True
 
-operator_list = "+-/*"
-rule = "="  
+while operator != "=":
+
+    while wait_for_number == True:
+        enter_operand = input("Enter number: ") 
+        
+        try:
+         operand = int(enter_operand)
+        except ValueError:
+            print(f"{operand} is not a number. Try again.")
+        else: 
+            wait_for_number = False 
+            continue       
+        
+    print(f"Pre Result: {result}")
+
+    while wait_for_number == False:
+        operator = input("Enter operator: ")    
+        if operator == '+' or operator == '*' or operator == '/' or operator == '-'or operator == "=":
+         wait_for_number = True
+        else:
+         print(f"{operator} is not '+' or '-' or '/' or '*'. Try again") 
+         continue
+    if operator == "+":
+        result += operand
+        print(f"Pre Result: {result}")
+    elif operator == "-":
+        result -= operand
+        print(f"Pre Result: {result}")
+    elif operator == "*":
+        result *= operand
+        print(f"Pre Result: {result}")
+    elif operator == "/":
+        result /= operand
+        print(f"Pre Result: {result}")
+    elif operator == "=":
+    
+       print(f"Result: {result}")
+       break
+    
 
 
-while wait_for_number == True:
-    operand = input("Enter number: ")
-    try:
-        operand = int(operand)
-    except ValueError:
-        print(f"{operand} is not a number. Try again.")
-    else: 
-        wait_for_number = False
-
-while wait_for_number == False:
-    operator = input("Enter operator: ")    
-    if operator == '+' or operator == '*' or operator == '/' or operator == '-':
-        wait_for_number = True
-    else:
-        print(f"{operator} is not '+' or '-' or '/' or '*'. Try again") 
-if operator == "+":
-    result += operand
-elif operator == "-":
-    result -= operand
-elif operator == "*":
-    result *= operand
-elif operator == "/":
-    result /= operand
-print(result)
+ #Перша: ["10", "+", "5", "6", "/", "3", "-", "a", "2", "*", "6", "= "], результат 18.0
+ #Друга: ["2", "3", "-", "1", "+", "10", "*", "2", "="], результат 22.0   
--- module1.py
+++ module1.py
@@ -1,3 +1,44 @@
 #my first lesson
-name = "Olha"
-hello_string = f"Hello, {name}"
+#is_nice = True
+#state = "nice" if is_nice else "not nice"
+#print(state)
+
+#temp = input("Enter your name: ")
+#name = temp if temp else "Anonimus"
+#print(f"Hi, {name}!")
+
+#money = int(input("enter your many: "))
+#count_money = money if money else "No money"
+#print(f"Dear {name}, you have {count_money}")
+
+fruit = 'apple'
+for char in fruit:
+    print(char)
+
+num = 1
+while num <= 5:
+    print(num)
+    num += 1
+
+a = 0
+while True:
+    if a >= 20:
+        break
+    print(a)
+    a = a + 1
+while True:
+    user_input = input()
+    print(user_input)
+    if user_input == "exit":
+        break
+while True:
+    number = input("number = ")
+    number = int(number)
+    while True:
+        print(number)
+        number = number - 1
+        if number < 0:
+            break
+
+
+
